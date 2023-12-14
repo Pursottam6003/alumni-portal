cbbfdf898b61494db84372ae682ca65ba6f7c69d
Pursottam Sah <rahulsah6003@gmail.com>
2023-12-14 11:29:35 +0530

    added a page update-profile.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, Login, Register } from "./views";
+import { Home, Login, Register, Update_Profile } from "./views";
 import Layout from "./components/layout";
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/update-profile" element={<Update_Profile />} />
+
         </Routes>
       </Layout>
     </BrowserRouter>
