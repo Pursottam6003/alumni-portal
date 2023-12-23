96fa5d69e09af07fbf09849b6799a4da912143c7
Chandrashekhar Tripathi <tripathics@outlook.com>
2023-12-24 00:37:02 +0530

    refactor profile navigation

--- src/components/profile-layout/index.js
+++ src/components/profile-layout/index.js
@@ -5,63 +5,61 @@ import cx from 'classnames'
 import { useLocation } from 'react-router-dom'
 import { useUser } from '../../contexts/UserContext'
 
-const ProfileLayout = ({ children }) => {
+const ProfileNavLink = ({ Icon, name, path, action = null, location }) => {
+  return (
+    <li>
+      <NavLink to={path}
+        onClick={(e) => {
+          if (action) action();
+        }}
+        className={cx(
+          styles['sidebar-nav-link'],
+          { [styles['active']]: location.pathname === path })
+        }>
+        {Icon && <Icon />}
+        {name}
+      </NavLink>
+    </li>
+  )
+}
+
+const ProfileNav = ({ title, links }) => {
   const location = useLocation();
+
+  return (
+    <nav className={`${styles["sidebar-nav"]}`}>
+      <div className={`${styles["sidebar-nav-header"]}`}>
+        <h2>{title}</h2>
+      </div>
+      <ul>
+        {links.map((link, index) => (
+          <ProfileNavLink location={location} key={index} {...link} />
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+const ProfileLayout = ({ children }) => {
   const { logout } = useUser();
 
   const profileLinks = [
-    { name: 'Personal details', path: '/profile', icon: UserIcon, },
-    { name: 'Academic details', path: '/profile/academic', icon: BookIcon, },
-    { name: 'Professional details', path: '/profile/professional', icon: SuitcaseIcon, },
+    { name: 'Personal details', path: '/profile', Icon: UserIcon, },
+    { name: 'Academic details', path: '/profile/academic', Icon: BookIcon, },
+    { name: 'Professional details', path: '/profile/professional', Icon: SuitcaseIcon, },
   ]
 
   const accountLinks = [
-    { name: 'Account settings', path: '/profile/account', icon: SettingsIcon, },
-    { name: 'Logout', path: '/', icon: LogOutIcon, action: logout }
+    { name: 'Account settings', path: '/profile/account', Icon: SettingsIcon, },
+    { name: 'Logout', path: '/', Icon: LogOutIcon, action: logout }
   ]
 
   return (
     <div className={`${styles["layout"]}`}>
       <div className={`${styles["sidebar"]}`}>
         <nav className={`${styles["sidebar-nav"]}`}>
-          <div className={`${styles["sidebar-nav-header"]}`}>
-            <h2>Alumni Profile</h2>
-          </div>
-          <ul>
-            {profileLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink to={link.path}
-                  className={cx(
-                    styles['sidebar-nav-link'],
-                    { [styles['active']]: location.pathname === link.path })
-                  }>
-                  <link.icon />
-                  {link.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-
-          <div className={`${styles["sidebar-nav-header"]}`}>
-            <h2>Account</h2>
-          </div>
-          <ul>
-            {accountLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink to={link.path}
-                onClick={(e) => {
-                  if (link.action) link.action();
-                }}
-                  className={cx(
-                    styles['sidebar-nav-link'],
-                    { [styles['active']]: location.pathname === link.path })
-                  }>
-                  <link.icon />
-                  {link.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+          <ProfileNav title="Alumni Profile" links={profileLinks} />
+          <ProfileNav title="Account" links={accountLinks} />
         </nav>
       </div>
 
