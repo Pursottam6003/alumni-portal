956e45f2ca7e042355d6db7246cdfd98bf00a8de
Chandrashekhar Tripathi <tripathics@outlook.com>
2023-12-26 23:23:04 +0530

    use promise for db query to catch err

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,24 +2,26 @@ const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT_SECRET;
 const db = require('../db/conn').getDb();
 
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
   const token = req.cookies.auth;
 
   try {
     const decoded = jwt.verify(token, SECRET);
-    
+
     // check if token has expired
     if (decoded.exp < Date.now() / 1000) throw new Error('Invalid jwt');
 
     // query for user
-    db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (err, results) => {
-      if (err) throw err;
-      if (results.length === 0) throw new Error('Invalid jwt');
-      req.user = results[0];
-      next();
+    const results = await new Promise((resolve, reject) => {
+      db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
     });
-  }
-  catch (err) {
+    if (results.length === 0) throw new Error('Invalid jwt');
+    req.user = results[0];
+    next();
+  } catch (err) {
     // log error
     console.log(err.message);
     if (err.message === 'Invalid jwt')
