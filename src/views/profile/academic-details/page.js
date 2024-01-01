363c1d2d5b06237e3d6b15926888019f17d15b0b
Pursottam Sah <rahulsah6003@gmail.com>
2024-01-02 00:07:31 +0530

    updated the education component

--- src/views/profile/academic-details/page.js
+++ src/views/profile/academic-details/page.js
@@ -1,12 +1,12 @@
 import cx from "classnames"
 import { SchemaForm, Button } from "../../../components/forms"
 import ModalComponent from "../../../components/forms/Modal/ModalComponent"
+import { EditPencil, PlusCircle as AddIcon } from 'iconoir-react'
 import styles from '../Profile.module.scss'
 import { useEffect, useState } from "react"
 
 const EducationFormNITAP = ({ onSubmit }) => {
   const prefillData = { institute: 'National Institute of Technology, Arunachal Pradesh' }
-
   return (
     <SchemaForm prefillData={prefillData} schema={[
       { name: 'institute', label: 'Institute', type: 'text', required: 'Institute is requried', disabled: true },
@@ -52,6 +52,53 @@ const EducationForm = ({ onSubmit }) => {
       <Button type="submit" className='primary'>Save</Button>
     )} />
   )
+}
+
+const EducationComponent = ({ data }, setIsModalOpen) => {
+  const degreeMap = new Map([
+    ["btech", "Bachelor of Technology"],
+    ["mtech", "Master of Technology"],
+    ["phd", "Doctor of Philosophy"]
+  ]);
+
+  const capitalizeFirstTwoLetters = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1, 2).toUpperCase() + str.slice(2);
+  }
+  return (
+    <div className={cx(styles['box-row'], styles.header)} >
+      <div className={cx(styles['logo-container'])}>
+        {data.institute === 'National Institute of Technology, Arunachal Pradesh' ? (
+          <img className={styles['logo']} src="/nitap-logo.svg" alt="nitap-logo" />
+        ) : (
+          <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/university.png" alt="university" />)}
+      </div>
+
+      <div className={cx(styles['box-column'])}>
+        <div className={cx(styles['college-name'], styles.value)}>
+          {data.institute}
+        </div>
+        <div className={cx(styles['course-details'], styles.label)}>
+          <p className={cx(styles['course-name'])}>{degreeMap.get(data.degree)} - {capitalizeFirstTwoLetters(data.degree)} in {data.discipline}</p>
+          <p className={cx(styles['course-duration'])}>{new Date(data.startDate).toLocaleString('default', { month: 'short' })} {new Date(data.startDate).getFullYear()} - {new Date(data.endDate).toLocaleString('default', { month: 'short' })} {new Date(data.endDate).getFullYear()}
+            {data.endDate > Date.now() ? " (Expected)" : ""}  {data.type === 'full-time' ? 'Full-Time' : 'Part-Time'}  </p>
+          <p className={cx(styles['course-grades'])}>Grade: A</p>
+        </div>
+        <div className={cx(styles['course-description'])}>
+          {data.courseDescription && <>
+            {data.courseDescription}
+          </>}
+        </div>
+      </div>
+
+      <div className={cx(styles['box-column'])} >
+        <Button className={cx(styles['editIcon'])} onClick={() => setIsModalOpen(true)}>
+          <EditPencil />Edit
+        </Button>
+      </div>
+    </div>
+  );
+
+
 }
 
 const AcademicDetails = () => {
@@ -139,13 +186,24 @@ const AcademicDetails = () => {
     </section>
     <section className={styles.box}>
       <div className={styles['box-table']}>
+
+        <div className={cx(styles['box-row'], styles.header)} >
+          <h3 className={styles['title']}>Academic Details</h3>
+
+          <Button onClick={() => setIsModalOpen(true)}>
+            <AddIcon />Add
+          </Button>
+        </div>
         <div className={cx(styles['box-row'])}>
           {educations.map(e => (
             <pre>
-              {JSON.stringify(e, null, 2)}
+              {/* {JSON.stringify(e, null, 2)} */}
+              <EducationComponent data={e} key={e.institute} setIsModalOpen={setIsModalOpen} />
             </pre>
           ))}
         </div>
+
+
       </div>
     </section>
   </>)
